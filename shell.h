82df9fc558bd0ccaf05a870e3ad80d8e4a3c161e
2023-23 <meryem.ag99@gmail.com>
2023-11-16 12:27:22 -0800

    uptodate

--- shell.h
+++ shell.h
@@ -1,45 +1,21 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <signal.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
 #include <errno.h>
 
-/*DECLARATIONS*/
+/* for read/write bufferes */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
 
-extern char **environ;
-
-/*INCLUDED FUNCTIONS*/
-char **string_storage(char *line);
-void path_finder(char **array);
-int execute_line(char *con, char **array);
-
-/*******CHECKS*******/
-void exit_check(char **array, char *line, int exit_stat);
-void env_check(char **array);
-int clear_check(char **array);
-void sigint_check(int sign);
-int space_tabs_check(char *line);
-/*******************/
-
-/*****ALLOCATION*****/
-void *_realloc(void *ptr, unsigned int size);
-/*******************/
-void free_2D(char **array);
-/*******STRING*******/
-int _putchar(char c);
-char *_strcpy(char *dest, char *src);
-char *_strcat(char *dest, char *src);
-int _strlen(char *s);
-int _strcmp(char *s1, char *s2);
-/********************/
-
-/****ADVANCED FUNCTION*****/
-ssize_t _getline(char **line, size_t *len, FILE *stream);
-/**************************/
-
-#endif
+/* for command chaining */
+#define CMD_NORM        0
