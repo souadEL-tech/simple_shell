0761bb8d76b76e7831232dbcf6186c4bdeb109b6
SOUAD_TECH <sou.elmarsani@gmail.com>
2023-11-17 16:36:03 -0800

    s_commit'

---
+++ exits.c
@@ -0,0 +1,84 @@
+#include "shell.h"
+
+/**
+ * _strncpy -finction
+ * @dest: destination
+ * @src: source
+ * @n: para
+ * Return: a pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+
+}
+
+
+/**
+ * *_strchr -function
+ * @s: string
+ * @c: catacter
+ * Return: a pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
+
+
+/**
+ * *_strncat - function
+ * @dest: destinaton
+ * @src: source
+ * @n: int number
+ *
+ * Return: prt
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+
+
